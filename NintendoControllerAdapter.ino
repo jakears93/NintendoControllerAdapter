64992e521e5d9c277099d711fce049157cbe9f9d
jake-laptop <jake.arsenault@hotmail.ca>
2018-07-23 18:39:06 -0400

    Progress on N64 controller read

--- NintendoControllerAdapter.ino
+++ NintendoControllerAdapter.ino
@@ -8,15 +8,16 @@
 /*
  *A basic inteface for NES, SNES and N64 controllers (GC to come).  Reports  1-3 controllers as a HID compliant device.
 */
-//REFERENCES
+//DEPENDENCIES
 /* 
- *Joystick Library by Matthew Heironimus.
+https://github.com/MHeironimus/ArduinoJoystickLibrary 
+delay_x.h
 */
 
 /*
 TODO Find better way to probe for controllers. In current implementation controllers will not be connected if the 'A' button is held down during read sequence.
 TODO Setup function to read n64 controller data
-TODO Implement hotswap funcionality to controllers via RESET flag
+TODO Implement timers in main loop and n64 data reads to accurately read signals and send reports at equal intervals
 */
 
 
@@ -25,20 +26,22 @@ TODO Implement hotswap funcionality to controllers via RESET flag
 //Define constants
 #define BC_NES 8	//NES button count
 #define BC_SNES 12	//SNES button count
-#define BC_N64 13	//N64 button count
-#define X_N64 4		//N64 X axis bit size
-#define Y_N64 4 	//N64 Y axis bit size
-#define DATA_NES 2	//NES data pin
-#define DATA_SNES 3	//SNES data pin
-#define DATA_N64 4	//N64 data pin
-#define LATCH 5		//Common controller latch signal pin
-#define CLOCK 6		//Common controller clock signal pin
+#define BC_N64 14	//N64 button count
+#define XAXIS_N64 4	//N64 X axis bit size
+#define YAXIS_N64 4 	//N64 Y axis bit size
+#define DATA_NES_PIN 2	//NES data pin
+#define DATA_SNES_PIN 3	//SNES data pin
+#define DATA_N64_PIN 4	//N64 data pin
+#define LATCH_PIN 5	//Common controller latch signal pin
+#define CLOCK_PIN 6	//Common controller clock signal pin
+#define RESET_PIN 0	//Reset flag pin
 
 //Setup global variables
-bool BOOTUP = 1; 	//Bootup flag
-bool RESET = 0;		//Reset flag
+bool BOOTUP = 1;	 	//Bootup flag
+bool RESET = 0;			//Reset flag
 byte ACTIVE_CONTROLLERS = 0x00;	//Bits 1-3 describe active status of NES/SNES/N64 controllers
-
+bool N64InputSignal[32] = {0};      //N64 controller data
+  
 //Declare 3 joystick objects
 Joystick_ nesCtrl(0x03, JOYSTICK_TYPE_GAMEPAD, BC_NES, 0, false, false, false, false, false, false, false, false, false, false, false);
 Joystick_ snesCtrl(0x03, JOYSTICK_TYPE_GAMEPAD, BC_SNES, 0, false, false, false, false, false, false, false, false, false, false, false);
@@ -48,20 +51,19 @@ void connectControllers(void)
 {
 //Pulse the latch pin on each controller and listen for response
 //Upon response, set active controller bits and initialize joysticks accordingly
-	digitalWrite(LATCH, 1);
-	digitalWrite(LATCH, 0);
-	if (digitalRead(DATA_NES))
+	digitalWrite(LATCH_PIN, 1);
+	digitalWrite(LATCH_PIN, 0);
+	if (digitalRead(DATA_NES_PIN)) 		//Fails if A button is pressed during latch
 	{
-		bitSet(ACTIVE_CONTROLLERS, 1);
+		bitSet(ACTIVE_CONTROLLERS, 1);	
 		nesCtrl.begin(0);
 	}
-	if (digitalRead(DATA_SNES))
+	if (digitalRead(DATA_SNES_PIN))		//Fails if A button is pressed during latch
 	{	
 		bitSet(ACTIVE_CONTROLLERS, 2);
 		snesCtrl.begin(0);
-	
 	}
-	if (digitalRead(DATA_N64))
+	if (digitalRead(DATA_N64_PIN))		//Fully functional, no changes needed
 	{
 		bitSet(ACTIVE_CONTROLLERS, 3);
 		n64Ctrl.begin(0);
@@ -73,15 +75,15 @@ void disconnectControllers(void)
 //Disconnect all controllers and return active controller byte to 0x00 (inactive)
 	if (bitRead(ACTIVE_CONTROLLERS, 1)==1)
 	{
-		nesCtrl.end(0);
+		nesCtrl.end();
 	}
 	if (bitRead(ACTIVE_CONTROLLERS, 2)==1)
 	{
-		snesCtrl.end(0);
+		snesCtrl.end();
 	}	
 	if (bitRead(ACTIVE_CONTROLLERS, 3)==1)
 	{
-		n64Ctrl.end(0);
+		n64Ctrl.end();
 	}
 	ACTIVE_CONTROLLERS = 0x00;
 }
@@ -92,20 +94,20 @@ void nesRead(bool FLAG)
 	if(FLAG)
 		return;
 
-	int buttons[BC_NES] = {0};
-	int previousState[BC_NES] = {0};
+	bool buttons[BC_NES] = {0};
+	bool previousState[BC_NES] = {0};
 	int COUNT = 0;
 
 // Latch current button status on controllers
-	digitalWrite(LATCH, 1);
-	digitalWrite(LATCH, 0);
-	buttons[COUNT] = !digitalRead(DATA_NES);
+	digitalWrite(LATCH_PIN, 1);
+	digitalWrite(LATCH_PIN, 0);
+	buttons[COUNT] = !digitalRead(DATA_NES_PIN);
 // Pulse the clock then read the remaining button data  
 	for (COUNT=1; COUNT<BC_NES; COUNT++)
 	{
-		digitalWrite(CLOCK, 1);
-		digitalWrite(CLOCK, 0);
-		buttons[COUNT] = !digitalRead(DATA_NES);
+		digitalWrite(CLOCK_PIN, 1);
+		digitalWrite(CLOCK_PIN, 0);
+		buttons[COUNT] = !digitalRead(DATA_NES_PIN);
 	}
 // Compare buttons states and fill button report  
 	for (COUNT=0; COUNT<BC_NES; COUNT++)
@@ -124,21 +126,19 @@ void snesRead(bool FLAG)
 //If flag is true, controller is inactive and will exit
 	if(FLAG)
 		return;
-
-	int buttons[BC_SNES] = {0};
-	int previousState[BC_SNES] = {0};
+	bool buttons[BC_SNES] = {0};
+	bool previousState[BC_SNES] = {0};
 	int COUNT = 0;
-
 // Latch current button status on controllers
-	digitalWrite(LATCH, 1);
-	digitalWrite(LATCH, 0);
-	buttons[COUNT] = !digitalRead(DATA_SNES);
+	digitalWrite(LATCH_PIN, 1);
+	digitalWrite(LATCH_PIN, 0);
+	buttons[COUNT] = !digitalRead(DATA_SNES_PIN);
 // Pulse the clock then read the remaining button data  
 	for (COUNT=1; COUNT<BC_SNES; COUNT++)
 	{
-		digitalWrite(CLOCK, 1);
-		digitalWrite(CLOCK, 0);
-		buttons[COUNT] = !digitalRead(DATA_SNES);
+		digitalWrite(CLOCK_PIN, 1);
+		digitalWrite(CLOCK_PIN, 0);
+		buttons[COUNT] = !digitalRead(DATA_SNES_PIN);
 	}
 // Compare buttons states and fill button report  
 	for (COUNT=0; COUNT<BC_SNES; COUNT++)
@@ -152,12 +152,143 @@ void snesRead(bool FLAG)
 	snesCtrl.sendState();
 }
 
+void N64_SIGNAL_HIGH() __attribute__((always_inline));
+void N64_SIGNAL_HIGH()
+{
+ _delay_us(1);          //Hold data low for 1 microseconds   
+ PORTD ^= B00000100;    //Toggle N64 data high
+ _delay_us(3);          //Hold data high for 3 microseconds
+ PORTD ^= B00000100;    //Toggle N64 data low
+}
+
+void N64_SIGNAL_LOW() __attribute__((always_inline));
+void N64_SIGNAL_LOW()
+{
+ _delay_us(3);          //Hold data low for 3 microseconds   
+ PORTD ^= B00000100;    //Toggle N64 data high
+ _delay_us(1);          //Hold data high for 1 microseconds
+ PORTD ^= B00000100;    //Toggle N64 data low
+}
+
+
 void n64Read(bool FLAG)
 {
 // If flag is true, controller is inactive and will exit
 	if (FLAG)
 		return;
 
+  // Reset controller data  
+  int COUNT = 0;
+  for(COUNT; COUNT<32; COUNT++)
+  {
+       N64InputSignal[COUNT] = 0; 
+  }
+  // Send intial polling sequence of 9 bits (0b000000011)
+  N64_SIGNAL_LOW(); 
+  N64_SIGNAL_LOW(); 
+  N64_SIGNAL_LOW(); 
+  N64_SIGNAL_LOW(); 
+  N64_SIGNAL_LOW(); 
+  N64_SIGNAL_LOW(); 
+  N64_SIGNAL_LOW(); 
+  N64_SIGNAL_HIGH();
+  N64_SIGNAL_HIGH();
+
+  DDRD ^= B00000100;      //Toggles N64 data pin to input via direction register
+  
+  //wait 1us then read bit 0, wait 4us read bit x (x++, x<32), loop isnt used to optimize speed
+  //changed to 0us to account for time to of toggling pins (toggles 2 times per signal, x 9 signals = 18 + 1 to toggle to input = 19 clock cycles or 19/16 us)
+  //dont need to account for reading signal as last 3us of 4us signal can be read for an accurate value and phase shift shouldnt exceed 32/16MHz = 2us
+  
+  //_delay_us(1);
+  N64InputSignal[0] = (PORTD & B00000100);   // Reads and stores only N64 data pin value by using logical AND expression
+  _delay_us(4);
+  N64InputSignal[1] = (PORTD & B00000100);  
+  _delay_us(4);
+  N64InputSignal[2] = (PORTD & B00000100);
+  _delay_us(4);
+  N64InputSignal[3] = (PORTD & B00000100);  
+  _delay_us(4);
+  N64InputSignal[4] = (PORTD & B00000100); 
+  _delay_us(4);
+  N64InputSignal[5] = (PORTD & B00000100);  
+  _delay_us(4);
+  N64InputSignal[6] = (PORTD & B00000100);  
+  _delay_us(4);
+  N64InputSignal[7] = (PORTD & B00000100);  
+  _delay_us(4);
+  N64InputSignal[8] = (PORTD & B00000100);  
+  _delay_us(4);
+  N64InputSignal[9] = (PORTD & B00000100);  
+  _delay_us(4);
+  N64InputSignal[10] = (PORTD & B00000100);  
+  _delay_us(4);
+  N64InputSignal[11] = (PORTD & B00000100);
+  _delay_us(4);
+  N64InputSignal[12] = (PORTD & B00000100);
+  _delay_us(4);
+  N64InputSignal[13] = (PORTD & B00000100);   
+  _delay_us(4);
+  N64InputSignal[14] = (PORTD & B00000100);   
+  _delay_us(4);
+  N64InputSignal[15] = (PORTD & B00000100);   
+  _delay_us(4);
+  N64InputSignal[16] = (PORTD & B00000100);   
+  _delay_us(4);
+  N64InputSignal[17] = (PORTD & B00000100);   
+  _delay_us(4);
+  N64InputSignal[18] = (PORTD & B00000100);   
+  _delay_us(4);
+  N64InputSignal[19] = (PORTD & B00000100);   
+  _delay_us(4);
+  N64InputSignal[20] = (PORTD & B00000100);
+  _delay_us(4);
+  N64InputSignal[21] = (PORTD & B00000100);
+  _delay_us(4);
+  N64InputSignal[22] = (PORTD & B00000100);
+  _delay_us(4);
+  N64InputSignal[23] = (PORTD & B00000100);
+  _delay_us(4);
+  N64InputSignal[24] = (PORTD & B00000100);
+  _delay_us(4);
+  N64InputSignal[25] = (PORTD & B00000100); 
+  _delay_us(4);
+  N64InputSignal[26] = (PORTD & B00000100); 
+  _delay_us(4);
+  N64InputSignal[27] = (PORTD & B00000100); 
+  _delay_us(4);
+  N64InputSignal[28] = (PORTD & B00000100); 
+  _delay_us(4);
+  N64InputSignal[29] = (PORTD & B00000100);  
+  _delay_us(4);
+  N64InputSignal[30] = (PORTD & B00000100);   
+  _delay_us(4);
+  N64InputSignal[31] = (PORTD & B00000100);   
+  DDRD ^= B00000100;                         //Returns N64 data pin to output for next function call
+
+// Fill gamepad report
+  COUNT = 0;
+  byte xAxis = 0;
+  byte yAxis = 0;
+  bool previousState[BC_N64+2];
+  int buttonNum = 0;
+
+  // Update button status if change has occured, skip reading bits 8 and 9 as they do not have any significance
+  for (COUNT=0; COUNT<BC_N64+2; COUNT++)
+  {
+    buttonNum++;
+    if (COUNT == 8 || COUNT == 9)
+    {
+      buttonNum--;  
+    }
+    else if (N64InputSignal[buttonNum] != previousState[buttonNum]){
+      n64Ctrl.setButton(buttonNum, N64InputSignal[buttonNum]); 
+      previousState[buttonNum] = N64InputSignal[buttonNum];
+    }
+  }
+  // Update joystick status if change has occured
+  //TODO
+  
 // Send gamepad report
 	n64Ctrl.sendState();
 }
@@ -167,13 +298,14 @@ void setup()
 //Initialize gpio pin modes
     if (BOOTUP)
     {
-        pinMode(DATA_NES, INPUT);
-        pinMode(DATA_SNES, INPUT);
-        pinMode(DATA_N64, INPUT);
-        pinMode(CLOCK, OUTPUT);
-        pinMode(LATCH, OUTPUT);
-        digitalWrite(CLOCK, 0);
-        digitalWrite(LATCH, 0);
+        pinMode(DATA_NES_PIN, INPUT);
+        pinMode(DATA_SNES_PIN, INPUT);
+        pinMode(DATA_N64_PIN, OUTPUT);
+        pinMode(CLOCK_PIN, OUTPUT);
+        pinMode(LATCH_PIN, OUTPUT);
+        digitalWrite(CLOCK_PIN, 0);
+        digitalWrite(LATCH_PIN, 0);
+        digitalWrite(DATA_N64_PIN, 0);
         BOOTUP = 0;   //Turn off boot flag
     }
     
@@ -195,8 +327,11 @@ void loop()
 	nesRead(bitRead(ACTIVE_CONTROLLERS, 1));
 	snesRead(bitRead(ACTIVE_CONTROLLERS, 2));
 	n64Read(bitRead(ACTIVE_CONTROLLERS, 3));
-// Delay to run at roughly 60Hz
-	delay(15);
+// Read reset pin for flag
+	if (!digitalRead(RESET_PIN))
+		RESET = 1;
+// Delay to run between roughly 60-100Hz per read/send
+	delay(10);
 }
 
 
